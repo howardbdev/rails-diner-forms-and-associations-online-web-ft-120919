d914e846ae5afbb7abfe7a3dd96a47f0870f3b9e
howardbdev <howarddevennish@gmail.com>
2020-03-05 11:38:48 -0500

    add custom validator to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,16 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items
   has_many :items, through: :order_items
+
+  # we want an order to have at least one item...
+  validate :order_must_have_at_least_one_item
+
+  # it needs to add an error to .errors!
+  def order_must_have_at_least_one_item
+    if item_ids.empty? # condition_fails
+      # add an error
+      errors.add(:items, "must exist -- choose at least one!")
+    end
+  end
+
 end
