84d30c15c4a251e63fc7482027c43c7175f61e45
howardbdev <howarddevennish@gmail.com>
2020-03-05 18:14:07 -0500

    menus new create show actions

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,28 @@
 class MenusController < ApplicationController
+
+  # get "/menus/new"
+  def new
+    @menu = Menu.new
+  end
+
+  # post "/menus"
+  def create
+    @menu = Menu.new(menu_params)
+
+    if @menu.save
+      redirect_to menu_path(@menu)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @menu = Menu.find(params[:id])
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name, items_attributes: [:price, :name, :description])
+  end
 end
