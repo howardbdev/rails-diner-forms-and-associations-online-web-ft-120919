8ab1afa73de755f2654c5463baebb0025d7e9896
howardbdev <howarddevennish@gmail.com>
2020-03-04 19:50:26 -0500

    build new and create actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,33 @@
 class OrdersController < ApplicationController
   def new
     @order = Order.new
+    @customers = Customer.all
+    @items = Item.all
   end
 
   def create
+    # {
+    #   "order"=>
+    #     {"customer_id"=>"1",
+    #       "order_item_ids"=>["", "1", "2"]},
+    #       "commit"=>"Create Order",
+    #       "controller"=>"orders",
+    #       "action"=>"create"}
 
+    @order = Order.new(
+      customer_id: params[:order][:customer_id],
+      item_ids: params[:order][:item_ids]
+    )
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render :new
+    end
   end
 
   def show
 
-  end 
+  end
+
+
 end
