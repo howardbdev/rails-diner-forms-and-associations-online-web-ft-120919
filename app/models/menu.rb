53eae80f0b0d03a040ab794819aff097a6a4e696
howardbdev <howarddevennish@gmail.com>
2020-03-06 12:40:32 -0500

    add accepts_nested_attributes

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,5 +3,10 @@ class Menu < ApplicationRecord
 
   validates :name, presence: true
 
-  accepts_nested_attributes_for :items
+
+  accepts_nested_attributes_for :items, reject_if: proc { |attributes| attributes['name'].blank? || attributes['description'].blank? || attributes['price'].blank? }
+
+  # def items_attributes=(attrs)
+  #   binding.pry
+  # end
 end
