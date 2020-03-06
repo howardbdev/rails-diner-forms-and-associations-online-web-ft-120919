13e016dc19b6bebead8d3c8e4e39d8be14602da1
howardbdev <howarddevennish@gmail.com>
2020-03-06 12:40:07 -0500

    add validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,6 @@ class Item < ApplicationRecord
   has_many :order_items
   has_many :orders, through: :order_items
 
-  validates :name, presence: true
+  validates :name, :price, :description, presence: true
 
 end
