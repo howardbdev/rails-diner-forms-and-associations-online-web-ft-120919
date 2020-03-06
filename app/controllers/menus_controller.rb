5fe5067bdd0c86562a77bd7e32e2657b97c9662e
howardbdev <howarddevennish@gmail.com>
2020-03-06 12:41:23 -0500

    update menus controller for multiple new items

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,12 +3,13 @@ class MenusController < ApplicationController
   # get "/menus/new"
   def new
     @menu = Menu.new
+    make_blank_items
   end
 
   # post "/menus"
   def create
     @menu = Menu.new(menu_params)
-
+    make_blank_items
     if @menu.save
       redirect_to menu_path(@menu)
     else
@@ -24,5 +25,9 @@ class MenusController < ApplicationController
 
   def menu_params
     params.require(:menu).permit(:name, items_attributes: [:price, :name, :description])
+  end
+
+  def make_blank_items
+    @new_items =  @menu.tap{|m| 3.times { m.items.build} }.items
   end
 end
